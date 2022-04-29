6a2c7c3254e6f2926638b48364add0d2b9c1ffeb
jaomisterio <joaogamerpolado@gmail.com>
2022-04-28 21:09:12 -0300

    fix bugs footer added

---
+++ script.js
@@ -0,0 +1,24 @@
+function displayInOut1(){
+    var vid = document.getElementById("vids1");
+    console.log(vid)
+    if (vid.style.display == "none") {
+        vid.style.display = "flex";
+       pageScroll();
+       return
+    } return vid.style.display = "none";
+}
+
+async function pageScroll() {
+    window.scrollBy(1, 100); // horizontal and vertical scroll increments
+    scrolldelay = await setTimeout(pageScroll,1);
+    clearTimeout(scrolldelay);
+    
+}
+
+async function scrollEnd(){
+    window.scrollBy(100, 10000); // horizontal and vertical scroll increments
+    scrolldelay = await setTimeout(ScrollEnd,1);
+    clearTimeout(scrolldelay);
+    
+}
+
